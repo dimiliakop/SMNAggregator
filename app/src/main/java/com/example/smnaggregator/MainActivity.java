2320a670641fc6dc1c3e37c28fdd9a91dd2a1fb5
dimiliakop <dai18127@uom.edu.gr>
2021-07-11 21:07:18 +0300

    11/07/22 Afternoon (Facebook login and basic app UI)

--- app/src/main/java/com/example/smnaggregator/MainActivity.java
+++ app/src/main/java/com/example/smnaggregator/MainActivity.java
@@ -19,10 +19,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
 
     @Override
